811dc3fc7996ee748acc5219cc8e38010e24403b
elmendacorp <elmendacorp@gmail.com>
2013-10-20 19:39:29 +0200

    Arreglados problemas con el texto procesado, ahora elimina bien espacios y saltos de linea

--- IC.py
+++ IC.py
@@ -9,31 +9,31 @@ import string
 # Hay 27 caracteres en el alfabeto
 ALPHABET_LOWERCASE = string.ascii_lowercase + "ñ"
 
-
 def cleanText(text):
     """
     Función creada para reemplazar los caracteres peculiares
     del lenguaje castellano
     """
-    text = text.replace("á", "a")
-    text = text.replace("é", "e")
-    text = text.replace("í", "i")
-    text = text.replace("ó", "o")
-    text = text.replace("ú", "u")
-    text = text.replace("ü", "u")
-    text = text.replace(",", " ")
-    text = text.replace(".", " ")
-    text = text.replace(";", " ")
-    text = text.replace(":", " ")
-    text = text.replace("!", " ")
-    text = text.replace("¡", " ")
-    text = text.replace("?", " ")
-    text = text.replace("¿", " ")
-    text = text.replace("(", " ")
-    text = text.replace(")", " ")
-    text = text.replace("/", " ")
-    text = text.replace("'", " ")
-    text = text.replace("-", " ")
+    text = text.replace('á', "a")
+    text = text.replace('é', "e")
+    text = text.replace('í', "i")
+    text = text.replace('ó', "o")
+    text = text.replace('ú', "u")
+    text = text.replace('ü', "u")
+    text = text.replace(",", "")
+    text = text.replace(".", "")
+    text = text.replace(";", "")
+    text = text.replace(":", "")
+    text = text.replace("!", "")
+    text = text.replace("¡", "")
+    text = text.replace("?", "")
+    text = text.replace("¿", "")
+    text = text.replace("(", "")
+    text = text.replace(")", "")
+    text = text.replace("/", "")
+    text = text.replace("'", "")
+    text = text.replace("-", "")
+    text = text.replace("\n", "")
     # Todo el texto a minuscula
     text = text.lower()
     return text
@@ -53,6 +53,7 @@ def countLetterFrecuency(text, textout):
     for i in ALPHABET_LOWERCASE:
         a= i + ": " + str(textStr.count(i))
         textout.write(a)
+        textout.write("\n")
 
 if __name__ == '__main__':
     fichero = open("test.txt")
@@ -61,13 +62,13 @@ if __name__ == '__main__':
     textStr = fichero.read()
 
     # Primero limpiamos el texto de los caracteres peculiares
+    
+    textStr = eraseWhiteSpace(textStr)
     textStr = cleanText(textStr)
-    print(textStr)
 
     # Segundo quitamos los espacios
-    textStr = eraseWhiteSpace(textStr)
+    
     fichero2.write(textStr)
-    print(textStr)
 
     countLetterFrecuency(textStr,fichero3)
     
