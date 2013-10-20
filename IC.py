27fba55cc6524fd01cf9c6f1184e7aae723b9fe6
josecolella <josecolella@yahoo.com>
2013-10-20 21:35:58 +0200

    Arreglado la función de contar la frecuencia

--- IC.py
+++ IC.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# coding: utf-8
 # Author: Jose Miguel Colella
 
 
@@ -9,17 +9,18 @@ import string
 # Hay 27 caracteres en el alfabeto
 ALPHABET_LOWERCASE = string.ascii_lowercase + "ñ"
 
+
 def cleanText(text):
     """
     Función creada para reemplazar los caracteres peculiares
     del lenguaje castellano
     """
-    text = text.replace('á', "a")
-    text = text.replace('é', "e")
-    text = text.replace('í', "i")
-    text = text.replace('ó', "o")
-    text = text.replace('ú', "u")
-    text = text.replace('ü', "u")
+    text = text.replace("á", "a")
+    text = text.replace("é", "e")
+    text = text.replace("í", "i")
+    text = text.replace("ó", "o")
+    text = text.replace("ú", "u")
+    text = text.replace("ü", "u")
     text = text.replace(",", "")
     text = text.replace(".", "")
     text = text.replace(";", "")
@@ -51,24 +52,23 @@ def eraseWhiteSpace(text):
 def countLetterFrecuency(text, textout):
     # Para contar la frecuencia de las letras usamos lo siguiente
     for i in ALPHABET_LOWERCASE:
-        a= i + ": " + str(textStr.count(i))
+        a = i + ": " + str(text.count(i))
         textout.write(a)
         textout.write("\n")
 
+
 if __name__ == '__main__':
     fichero = open("test.txt")
-    fichero2 = open("process.txt","w")
-    fichero3 = open ("count.txt", "w")
+    fichero2 = open("process.txt", "w")
+    fichero3 = open("count.txt", "w")
     textStr = fichero.read()
 
     # Primero limpiamos el texto de los caracteres peculiares
-    
-    textStr = eraseWhiteSpace(textStr)
     textStr = cleanText(textStr)
+    textStr = eraseWhiteSpace(textStr)
 
     # Segundo quitamos los espacios
-    
+
     fichero2.write(textStr)
 
-    countLetterFrecuency(textStr,fichero3)
-    
+    countLetterFrecuency(textStr, fichero3)
